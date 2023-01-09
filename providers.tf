b1242fb33b0803c73a851e7383c0b3793ee7e908
Anurag Singh <anuragsingh.in@gmail.com>
2023-01-09 17:06:19 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
+    host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.this.token
 
@@ -10,7 +10,7 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
+  host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.this.token
 
