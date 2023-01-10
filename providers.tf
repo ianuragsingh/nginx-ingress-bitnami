d878f4feacc265a199b6d5f96765a8e8a40cc8ac
Anurag Singh <anuragsingh.in@gmail.com>
2023-01-10 11:00:31 +0800

    Reverting earlier change

--- providers.tf
+++ providers.tf
@@ -2,7 +2,6 @@
 
 provider "helm" {
   kubernetes {
-    load_config_file       = false
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.this.token
@@ -11,7 +10,6 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  load_config_file       = false
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.this.token
